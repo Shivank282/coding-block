b4692b04a96c4938376cf820bc2d5e7cc71472a4
Shivank282 <shivankbhargav98@gmail.com>
2026-04-01 14:08:43 +0530

    leetcode

---
+++ Coding Block/Assignment-4/Product of Array Except Self.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long arr[n], output[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    output[0] = 1;
+    for(int i = 1; i < n; i++) {
+        output[i] = output[i - 1] * arr[i - 1];
+    }
+
+    long long right = 1;
+    for(int i = n - 1; i >= 0; i--) {
+        output[i] = output[i] * right;
+        right = right * arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << output[i] << " ";
+    }
+
+    return 0;
+}
